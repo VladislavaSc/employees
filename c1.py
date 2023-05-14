6b6c7e8425c35d08f3295a8c8eef3a2213b5e5e5
VladislavaSc <133577867+VladislavaSc@users.noreply.github.com>
2023-05-14 16:43:41 +0300

    Update c1.py

--- c1.py
+++ c1.py
@@ -1,1 +1,15 @@
+class Employee:
 
+    def __init__(self, name, surname, pay):
+        self.name = name
+        self.surname = surname
+        self.email = f'{self.name}.{self.surname}@myjobmail.com'
+        self.pay = pay
+
+emp1 = Employee('Ivan', 'Ivanov', 60000)
+
+print(emp1)
+print(emp1.name)
+print(emp1.surname)
+print(emp1.pay)
+print(emp1.email)
