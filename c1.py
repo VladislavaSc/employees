84d5c4fcc3a9b673a74ad0360ceb6a7c8dca29cf
Vladislava Fridmann <vladislava.fridmann@gmail.com>
2023-05-14 19:12:45 +0300

    Commit

--- c1.py
+++ c1.py
@@ -1,4 +1,5 @@
 class Employee:
+    raise_coef = 1.5
 
     def __init__(self, name, surname, pay):
         self.name = name
@@ -6,8 +7,18 @@ class Employee:
         self.email = f'{self.name.lower()}.{self.surname.lower()}@myjobmail.com'
         self.pay = pay
 
+    def raise_pay(self):
+        self.pay *= self.raise_coef
+
 if __name__ == '__c1__':
     emp1 = Employee('Ivan', 'Ivanov', 60000)
 
-# TestCase#1 Email
-    assert emp1.email == 'Ivan.Ivanov@myjobmail.com'
+    # TestCase#1 Email
+    assert emp1.email == 'ivan.ivanov@myjobmail.com'
+
+    # TestCase#2 RaisePay
+    emp1.raise_pay()
+
+    assert 60000 * Employee.raise_coef == emp1.pay
+
+    assert emp1.pay == 90000
