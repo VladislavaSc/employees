5eda220176e735c41c7fbed6d98e0d8511177fd2
VladislavaSc <vladislava.fridmann@gmail.com>
2023-06-01 10:37:10 +0300

    Commit

--- exception_propagation.py
+++ exception_propagation.py
@@ -1,18 +1,26 @@
 class TryExec:
 
     def func1(self):
-        self.func2()
+        try:
+            self.func2()
+        except ZeroDivisionError:
+            print('The occurrence of an error in the method func1')
         print('Regular work of method func1')
 
 
     def func2(self):
-        self.func3()
+        try:
+            self.func3()
+        except ZeroDivisionError:
+            print('The occurrence of an error in the method func2')
         print('Regular work of method func2')
 
 
-
     def func3(self):
-        100 / 0
+        try:
+            100 / 0
+        except ZeroDivisionError:
+            print('The occurrence of an error in the method func3')
         print('Regular work of method func3')
 
 
